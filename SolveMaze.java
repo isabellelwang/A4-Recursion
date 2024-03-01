8d78b0fd675c2a0f8e85d9c7cbdb11bc34fb0017
Isabelle Wang <ilwang@smith.edu>
2024-03-01 11:05:24 -0500

    New

---
+++ SolveMaze.java
@@ -0,0 +1,17 @@
+class SolveMaze {
+  boolean finished = false; 
+  public boolean mazeSolver() {
+    
+
+
+
+
+  }
+  public static void main(String[] args) {
+    Maze maze = new Maze();
+    MazeViewer viewer = new MazeViewer(maze);
+    maze.initDemoMaze();
+
+
+  }
+}
